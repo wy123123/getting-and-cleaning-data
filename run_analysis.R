0c02965882ba3b40dd370650a0982cdbe489bd80
wy123123 <wangyu0917@hotmail.com>
2015-03-10 16:55:15 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,41 @@
+#read train data
+library(data.table)
+#please enter the dir where you extract the zip file
+#e.g, if i extract the zip file to c:/, then dir="c:/UCI HAR Dataset".
+dir="c:/UCI HAR Dataset"
+train.label=read.table(paste(dir,"/train/y_train.txt",sep=""))
+train.x=read.table(paste(dir,"/train/x_train.txt",sep=""))
+subject.train=read.table(paste(dir,"/train/subject_train.txt",sep=""))
+
+#read testing data
+test.label=read.table(paste(dir,"/test/y_test.txt",sep=""))
+test.x=read.table(paste(dir,"/test/x_test.txt",sep=""))
+subject.test=read.table(paste(dir,"/test/subject_test.txt",sep=""))
+
+#read feature and activity label
+dt.feature=read.table(paste(dir,"/features.txt",sep=""))
+dt.activity=read.table(paste(dir,"/activity_labels.txt",sep=""))
+
+#combine test and training data.
+dt.all=cbind(c(subject.train$V1,subject.test$V1),c(train.label$V1,test.label$V1),rbind(train.x,test.x))
+colnames(dt.all)[1:30]
+
+#name the vairables according to feature
+setnames(dt.all,c("Subject","Activity",as.character(dt.feature$V2)))
+
+# get the activity
+dt.all$Activity=dt.activity$V2[match(dt.all$Activity_label,dt.activity$V1)]
+
+#find data of mean and std
+dt_mean=dt.all[,grepl("mea",colnames(dt.all))]
+dt_std=dt.all[,grepl("std",colnames(dt.all))]
+#combine data
+dt_mean_std=data.table(cbind(dt.all$Subject,dt.all$Activity,dt_mean,dt_std))
+setnames(dt_mean_std,1:2,c("Subject","Activity"))
+
+#find average
+dt_avg=dt_mean_std[,lapply(.SD,mean,na.rm=T),by=list(Subject,Activity)]
+
+#output file
+write.table(dt_avg,file=paste(dir,"tidy.txt",sep=""),row.names=F)
 
